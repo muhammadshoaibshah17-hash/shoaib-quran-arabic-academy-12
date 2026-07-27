d641a7268c32d136e5f82507fd77853ff943e7e5
Muhammad Shoaib <muhammadshoaibshah17@gmail.com>
2026-07-28 01:50:39 +0500

    Remove next-intl setup completely - was causing redirect loop

--- app/[locale]/page.tsx
+++
@@ -1,5 +0,0 @@
-import Home from "../page";
-
-export default function LocalePage() {
-  return <Home />;
-}
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 };
 
-export default withNextIntl(nextConfig);
+export default nextConfig;
+
